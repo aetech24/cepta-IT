0bc8d7721f8f9bc86a2293f1dfc11b960c600def
Stedan Ampofo <stedanampofo19@gmail.com>
2024-12-13 22:57:15 +0000

    commit

--- src/comp/nav.js
+++ src/comp/nav.js
@@ -3,8 +3,11 @@ import './nav.css'
 import { MdLocalShipping } from "react-icons/md";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FiLogIn } from "react-icons/fi";
+import { CiLogout } from "react-icons/ci";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Nav = () => {
+    const { loginWithRedirect, logout, user, isAuthenticated} = useAuth0();
     return(
         <>
         <div className="header">
@@ -25,14 +28,28 @@ const Nav = () => {
                     <input type="text" value='' placeholder="search"></input>
                     <button><AiOutlineSearch /></button>
                 </div>
+                {
+                    isAuthenticated ?
+                    //Logout button
+                    <div className="user">
+                        <div className="icon">
+                            <CiLogout />
+                        </div>
+                        <div className='btn'>
+                            <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Logout</button>
+                        </div>
+                    </div>
+                :
+                //Login button
                 <div className="user">
                     <div className="icon">
-                    <FiLogIn />
+                        <FiLogIn />
                     </div>
                     <div className='btn'>
-                        <button>Login</button>
+                        <button onClick={() => loginWithRedirect()}>Login</button>
                     </div>
                 </div>
+                }
             </div>
         </div>
         </>
