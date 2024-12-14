58c5f82ff28e21c45d54cc7bfed0b559590e91fd
Stedan Ampofo <stedanampofo19@gmail.com>
2024-12-14 00:37:09 +0000

    nav

--- src/comp/nav.js
+++ src/comp/nav.js
@@ -4,7 +4,9 @@ import { MdLocalShipping } from "react-icons/md";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FiLogIn } from "react-icons/fi";
 import { CiLogout } from "react-icons/ci";
+import { CiUser } from "react-icons/ci";
 import { useAuth0 } from "@auth0/auth0-react";
+import {Link} from 'react-router-dom';
 
 const Nav = () => {
     const { loginWithRedirect, logout, user, isAuthenticated} = useAuth0();
@@ -30,7 +32,7 @@ const Nav = () => {
                 </div>
                 {
                     isAuthenticated ?
-                    //Logout button
+                    //if user is logged in, then Logout button will show and also user profile
                     <div className="user">
                         <div className="icon">
                             <CiLogout />
@@ -40,7 +42,7 @@ const Nav = () => {
                         </div>
                     </div>
                 :
-                //Login button
+                //if user is not logged in, then Login button will show
                 <div className="user">
                     <div className="icon">
                         <FiLogIn />
@@ -50,6 +52,44 @@ const Nav = () => {
                     </div>
                 </div>
                 }
+            </div>
+            <div className="last_header">
+                <div className="user_profile">
+                    {
+                        // user profile will show here
+                        isAuthenticated ?
+                        <>
+                        <div className="icon">
+                        <CiUser />
+                        </div>
+                        <div className='info'>
+                            <h2>{user.name}</h2>
+                            <p>{user.email}</p>                            
+                        </div>
+                        </>
+                        :
+                        <>
+                        <div className="icon">
+                            <CiUser />
+                        </div>
+                        <div className='info'>
+                            <p>Please Login</p>
+                        </div>
+                        </>
+                    }
+                </div>
+                <div className='nav'>
+                    <ul>
+                        <li><Link to='/' className="link">Home</Link></li>
+                        <li><Link to='/shop' className="link">Shop</Link></li>
+                        <li><Link to='/collection' className="link">Collection</Link></li>
+                        <li><Link to='/about' className="link">About</Link></li>
+                        <li><Link to='/contact' className="link">Contact</Link></li>
+                    </ul>
+                </div>
+                <div className='offer'>
+                    <p>flat 10$ over all iPhones</p>
+                </div>
             </div>
         </div>
         </>
